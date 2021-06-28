8d5587275cfbc9bbb46b7e87f4a786da901a059e
Atieh Kashani <atieh.kashani@gmail.com>
2021-06-28 12:13:13 -0700

    Delete Machine Learning/HW1 directory

--- Machine Learning/HW1/log(1-x)Function.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pylab as plt
-import numpy as np
-x = np.arange(-10, 0, 0.01)
-y = np.log10(1-x)
-plt.plot(x, -y)
-plt.title('y=-log(1-x)')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.show()
--- Machine Learning/HW1/log(x)Function.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pylab as plt
-import numpy as np
-x = np.arange(1, 10, 0.01)
-f = np.log10(x)
-plt.plot(x, -f)
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('y=-log(x)')
-plt.show()
